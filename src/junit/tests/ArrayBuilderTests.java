84ea4f7de06d9e68be8e5b74a3086079fbee79fd
Tom M-pc <105756196+TomMcDonaldMetro@users.noreply.github.com>
2023-04-20 17:08:32 -0500

    Added tests for 2d array builder.

--- src/junit/tests/ArrayBuilderTests.java
+++ src/junit/tests/ArrayBuilderTests.java
@@ -1,5 +1,6 @@
 package junit.tests;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 import java.util.Arrays;
@@ -38,6 +39,30 @@ class ArrayBuilderTests {
 		double[] actualStringArrayAfterBuild = Arrays.stream(ArrayBuilder.buildArray(sampleInput)).mapToDouble(Double::parseDouble).toArray();
 		assertArrayEquals(expectedArrayAfterBuild, actualStringArrayAfterBuild);
 		
+	}
+	
+	@Test
+	void Build2dStringArray() throws Exception {
+		String sampleInput = "[[1,2,3],[4,5,6],[7,8,9]]";
+		String[][] expectedArrayAfterBuild = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
+		String[][] actualStringArrayAfterBuild = ArrayBuilder.string2dString(sampleInput);
+		assertTrue(Arrays.deepEquals(expectedArrayAfterBuild, actualStringArrayAfterBuild));
+	}
+	
+	@Test
+	void Build2dIntegerArray() throws Exception {
+		String sampleInput = "[[1,2,3],[4,5,6],[7,8,9]]";
+		int[][] expectedArrayAfterBuild = {{1,2,3},{4,5,6},{7,8,9}};
+		int[][] actualStringArrayAfterBuild = ArrayBuilder.string2dInteger(sampleInput);
+		assertTrue(Arrays.deepEquals(expectedArrayAfterBuild, actualStringArrayAfterBuild));
+	}
+	
+	@Test
+	void Build2dDoubleArray() throws Exception {
+		String sampleInput = "[[1.5,2.3,3.7],[4.2,5.4,6.2],[7.7,8.5,9.9]]";
+		double[][] expectedArrayAfterBuild = {{1.5,2.3,3.7},{4.2,5.4,6.2},{7.7,8.5,9.9}};
+		double[][] actualStringArrayAfterBuild = ArrayBuilder.string2dDouble(sampleInput);
+		assertTrue(Arrays.deepEquals(expectedArrayAfterBuild, actualStringArrayAfterBuild));
 	}
 
 }
