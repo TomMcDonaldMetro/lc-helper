70dd93054592bb8bf2fe477b7e64f8c510d0261b
Tom M-pc <105756196+TomMcDonaldMetro@users.noreply.github.com>
2023-04-20 11:48:55 -0500

    added 2d array builder to ArrayBuilder class

--- src/com/helper/arrays/ArrayBuilder.java
+++ src/com/helper/arrays/ArrayBuilder.java
@@ -1,6 +1,8 @@
 package com.helper.arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 
 public class ArrayBuilder {
 	
@@ -16,6 +18,7 @@ public class ArrayBuilder {
 	public static String[] buildArray(String s) {
 		
 		ArrayList<String> arr = new ArrayList<>();
+		
 		String temp = "";
 		
 		for(int i = 0; i < s.length(); i++) {
@@ -36,6 +39,77 @@ public class ArrayBuilder {
 		}
 		
 		return arr.toArray(new String[arr.size()]);
+	}
+	
+	public static String[][] build2dArray(String s) throws Exception {
+		
+		ArrayList<ArrayList<String>> arrArr = new ArrayList<ArrayList<String>>();
+		Stack<Character> stack = new Stack<>();
+		String temp = "";
+		
+		s = s.replaceAll("\\s",""); // remove white space that may have entered.
+			
+		ArrayList<String> tempArr = new ArrayList<>();
+		int index = 0;
+		
+		if(s.charAt(index) == '[') {
+			stack.push(s.charAt(index));
+			index++;
+			
+		} else {
+			throw new Exception("Invalid Input: " + s.charAt(index) + " at position - " + index + " invalid format.");
+			
+		}
+		
+		while(!stack.isEmpty()) {
+			
+			if(s.charAt(index) == '[') {
+				stack.push(s.charAt(index));
+				
+			}else if(s.charAt(index) == ',') {
+				tempArr.add(temp);
+				
+				if(stack.size() == 1) {
+					arrArr.add(tempArr);
+					tempArr = new ArrayList<String>();
+
+				}
+				
+				temp = "";
+				
+			}else if(s.charAt(index) == ']') {
+				stack.pop();
+				
+			}else {
+				temp += s.charAt(index);
+				
+			}
+			
+			index++;
+			
+		}
+		
+		// final element and array being inserted.
+		tempArr.add(temp);
+		arrArr.add(tempArr); 
+		
+		String[][] res = new String[arrArr.size()][];
+		for(int i = 0; i < arrArr.size(); i++) {
+			ArrayList<String> row = arrArr.get(i);
+			res[i] = row.toArray(new String[row.size()]);
+		}
+		
+		return res;
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		String s = "[[1,2,3],[4,5,6],[7,8,9]]";
+		
+		System.out.println(Arrays.deepToString(build2dArray(s)));
+		
+		
 	}
 	
 }
